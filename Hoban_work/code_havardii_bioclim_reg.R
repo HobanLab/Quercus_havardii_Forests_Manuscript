12ae32f7764de38bc496dbcfca37e875b5b2ce31
Sean <shoban@alumni.nd.edu>
2019-04-12 11:17:30 -0500

    condensing the individual lists of plots etc. into a loop over the significant values

--- Hoban_work/code_havardii_bioclim_reg.R
+++ Hoban_work/code_havardii_bioclim_reg.R
@@ -36,51 +36,41 @@ reg_r2<-matrix(nrow=dim(clim)[2],ncol=5)
 for (ss in 1:length(stat_list)){
 	for (c in 1:dim(clim)[2]){
 		reg_pval[c,ss]<-summary(lm(gen_sum_stats[,stat_list[ss]]~clim[,c]))[4][[1]][8]
-		reg_r2<-summary(lm(gen_sum_stats[,stat_list[ss]]~clim[,c]))[8]	
+		reg_r2[c,ss]<-unlist(summary(lm(gen_sum_stats[,stat_list[ss]]~clim[,c]))[8]	)
 	}
 }
- matrix(p.adjust(reg_pval,"BH"),nrow=12,ncol=5)
- 
- which(p.adjust(reg_pval,"BH")<0.05)%%12
- [1]  2  3 10  2  3  8  9 10  1  2  8 10
+ #identify rows and columns with significant p values 
+ row_col_signif<-which( matrix(p.adjust(reg_pval,"BH"),nrow=12,ncol=5)<0.05,arr.ind=T)
  
  sort(reg_r2)
  
-  setwd("C:/Users/shoban/Documents/GitHub/QH_EnvironmentalAnalyses/")
-list_bioc<-read.csv("list_bioclim_var.csv")
+setwd("C:/Users/shoban/Documents/GitHub/QH_EnvironmentalAnalyses/")
+ #read in the names of the bioclim variables for the axis names of the plots
+ #the x is for those variables to be removed due to correlation
+ list_bioc<-read.csv("list_bioclim_var.csv")
  list_bioc<-list_bioc[list_bioc[,1]=="x"]
  list_bioc<-list_bioc[-which(list_bioc[,1]=="x"),]
  
- par(mfrow=c(4,3),mar=c(3,3,2,2),oma=c(2,3,1,1))
- plot(gen_sum_stats[,6]~clim[,2])
- plot(gen_sum_stats[,6]~clim[,3])
- plot(gen_sum_stats[,6]~clim[,10])
- plot(gen_sum_stats[,8]~clim[,2])
- plot(gen_sum_stats[,8]~clim[,3])
- plot(gen_sum_stats[,8]~clim[,8])
- plot(gen_sum_stats[,8]~clim[,9])
- plot(gen_sum_stats[,8]~clim[,10])
- plot(gen_sum_stats[,9]~clim[,1])
- plot(gen_sum_stats[,9]~clim[,2])
- plot(gen_sum_stats[,10]~clim[,8])
- plot(gen_sum_stats[,10]~clim[,10])
- 
- matrix(p.adjust(reg_pval,"BY"),nrow=12,ncol=5)
- pdf(width=10, height=4,file="regr_gen_clim.pdf")
- par(mfrow=c(1,3))
+ #this will cycle through all signification associations (row_col_signif) and plot that regression, pulling from row_col_signif
+ par(mfrow=c(4,3),mar=c(4,4,2,2),oma=c(2,3,1,1))
+ for (i in 1:nrow(row_col_signif)){
+  eq1<-gen_sum_stats[,stat_list[row_col_signif[i,2]]]~clim[,row_col_signif[i,1]]
+ plot(eq1,col=c(rep("red",13),rep("blue",13)),pch=19, xlab=list_bioc[row_col_signif[i,1],3], ylab=colnames(gen_sum_stats)[stat_list[row_col_signif[i,2]]])
+ abline(lm(eq1))
+ mtext(paste("R2= ",round(unlist(summary(lm(eq1))[8]),3),sep=""),side=3)
+ # text(eq1, labels = gen_sum_stats[,2],cex=2) #optional to add labels for pop names
 
- plot(gen_sum_stats[,6]~clim[,10],col=c(rep("red",13),rep("blue",13)),pch=19, xlab=list_bioc[10,3], ylab=colnames(gen_sum_stats)[6])
-	text((gen_sum_stats[,6]-.005)~clim[,10], labels = gen_sum_stats[,2])
- abline(lm(gen_sum_stats[,6]~clim[,10]))
- 	  text(45,.72,paste("R2= ",round(unlist(summary(lm(gen_sum_stats[,6]~clim[,10]))[8]),3),sep=""))
-plot(gen_sum_stats[,8]~clim[,10],col=c(rep("red",13),rep("blue",13)),pch=19, xlab=list_bioc[10,3], ylab=colnames(gen_sum_stats)[8])
- 	text((gen_sum_stats[,8]-.5)~clim[,10], labels = gen_sum_stats[,2])
-	abline(lm(gen_sum_stats[,8]~clim[,10]))
- 	  text(50,58,paste("R2= ",round(unlist(summary(lm(gen_sum_stats[,8]~clim[,10]))[8]),3),sep=""))
-plot(gen_sum_stats[,9]~clim[,2],col=c(rep("red",13),rep("blue",13)),pch=19, xlab=list_bioc[2,3], ylab=colnames(gen_sum_stats)[9])
-	text((gen_sum_stats[,9]+.001)~clim[,2], labels = gen_sum_stats[,2])
-	abline(lm(gen_sum_stats[,9]~clim[,2]))
-	  text(40,.13,paste("R2= ",round(unlist(summary(lm(gen_sum_stats[,9]~clim[,2]))[8]),3),sep=""))
-	dev.off()
-	
-	
+ }
+ 
+ row_col_signif<-which( matrix(p.adjust(reg_pval,"BY"),nrow=12,ncol=5)<0.05,arr.ind=T)
+# pdf(width=10, height=4,file="regr_gen_clim.pdf")
+ par(mfrow=c(1,3))
+ for (i in 1:nrow(row_col_signif)){
+ eq1<-gen_sum_stats[,stat_list[row_col_signif[i,2]]]~clim[,row_col_signif[i,1]]
+ plot(eq1,col=c(rep("red",13),rep("blue",13)),pch=19, xlab=list_bioc[row_col_signif[i,1],3], ylab=colnames(gen_sum_stats)[stat_list[row_col_signif[i,2]]])
+ abline(lm(eq1))
+ mtext(paste("R2= ",round(unlist(summary(lm(eq1))[8]),3),sep=""),side=3)
+ # text(eq1, labels = gen_sum_stats[,2],cex=2) #optional to add labels for pop names
+
+ }
+ 
