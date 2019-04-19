f4343e3d015e0371da9b88ff901b882d7a9d5c55
Sean <shoban@alumni.nd.edu>
2019-04-19 16:47:03 -0500

    code to tailor the environmental variables to each region, and to restrict plotting to those with row_col !=0, and new plots

--- Hoban_work/code_havardii_bioclim_reg_notworking.R
+++ Hoban_work/code_havardii_bioclim_reg_notworking.R
@@ -11,8 +11,6 @@ setwd("C:/Users/shoban/Documents/GitHub/QH_EnvironmentalAnalyses/Hoban_work/")
  QHloc <- read.csv("Hob_QHOccur_gen.csv", sep=",", header=T, stringsAsFactors = FALSE) #Loading locations
 
 clim <- extract(bioclim2.5, QHloc[,1:2]) 
-#subtract the correlated variables- TO REDO ONCE HAVE FINAL LIST... MOVE BELOW
-clim<-clim[,-c(1,5,6,13,14,16,18)]
 
 #add in lat/long- optional... I would argue that lat long are not biologically relevant here
 #clim_and_loc<-cbind(clim, QHloc[,1:2])
@@ -42,27 +40,33 @@ setwd("C:/Users/shoban/Documents/GitHub/QH_EnvironmentalAnalyses/Hoban_work/")
 pop_set<-list(1:13,14:26,1:26,15:26)
 point_colors<-c(rep("red",13),rep("blue",13))
 point_shapes<-c(rep(21,13),rep(24,13))
+plot_col<-4
 
 #Loop to go over East, West, East+West Together, and West without W-1
 for (p in 1:4){
 	#store name of region we are in for writing file names
-	if (p==1) region<-"E"; if (p==2) region<-"W"; if (p==3) region<-"overall"; if (p==4) region<-"W_sub"
+	#subtract the correlated variables clim<-clim[,-c(1,5,6,13,14,16,18)]
+
+	if (p==1) { region<-"E"; clim_to_keep<-c(1,3,4,6,11,15,18); clim_reg<-clim[,clim_to_keep] }
+	if (p==2) { region<-"W"; clim_to_keep<-c(2,3,7,9,10,14,17,18,19); clim_reg<-clim[,clim_to_keep] }
+	if (p==3) { region<-"overall"; clim_to_keep<-c(2:4,7:10,12,15,17,19); clim_reg<-clim[,clim_to_keep] }
+	if (p==4) { region<-"W_sub"; clim_to_keep<-c(2,3,7,9,10,14,17,18,19); clim_reg<-clim[,clim_to_keep] }
 	
 	#place to keep regression results (p values) of all regressions run
-	reg_pval<-matrix(nrow=dim(clim)[2],ncol=5)
-	reg_r2<-matrix(nrow=dim(clim)[2],ncol=5)
+	reg_pval<-matrix(nrow=dim(clim_reg)[2],ncol=5)
+	reg_r2<-matrix(nrow=dim(clim_reg)[2],ncol=5)
 
 	#linear models
 	for (ss in 1:length(stat_list)){
-		for (c in 1:dim(clim)[2]){
-			reg_pval[c,ss]<-summary(lm(gen_sum_stats[pop_set[[p]],stat_list[ss]]~clim[pop_set[[p]],c]))[4][[1]][8]
-			reg_r2[c,ss]<-unlist(summary(lm(gen_sum_stats[pop_set[[p]],stat_list[ss]]~clim[pop_set[[p]],c]))[8]	)
+		for (c in 1:dim(clim_reg)[2]){
+			reg_pval[c,ss]<-summary(lm(gen_sum_stats[pop_set[[p]],stat_list[ss]]~clim_reg[pop_set[[p]],c]))[4][[1]][8]
+			reg_r2[c,ss]<-unlist(summary(lm(gen_sum_stats[pop_set[[p]],stat_list[ss]]~clim_reg[pop_set[[p]],c]))[8]	)
 		}
 	}
 	
 	#identify rows and columns with significant p values 
-	 	row_col_signif<-which( matrix(p.adjust(reg_pval,"BH"),nrow=12,ncol=5)<0.05,arr.ind=T)
-		if (p==4) row_col_signif<-which( matrix(p.adjust(reg_pval,"BH"),nrow=12,ncol=5)<0.075,arr.ind=T)
+	 	row_col_signif<-which( matrix(p.adjust(reg_pval,"BH"),nrow=length(clim_to_keep),ncol=5)<0.05,arr.ind=T)
+		if (p==4) row_col_signif<-which( matrix(p.adjust(reg_pval,"BH"),nrow=length(clim_to_keep),ncol=5)<0.075,arr.ind=T)
 	#store the list of which variables are significant and what genetic statistic
 		variables_id<-matrix(nrow=length(row_col_signif[,1]),ncol=2)
 		for (l in 1:length(row_col_signif[,1])) variables_id[l,1]<-as.character(list_bioc[row_col_signif[l,1],3])
@@ -74,33 +78,31 @@ for (p in 1:4){
 	 #this will cycle through all signification associations (row_col_signif) and plot that regression, pulling from row_col_signif
 	if (p!=2) {
 		pdf(width=10, height=10,file=paste0("regr_gen_clim_loose_",region,".pdf"))
-		if (p==1) par(mfrow=c(4,4),mar=c(4,4,2,2),oma=c(2,3,1,1))
-		if (p==3) par(mfrow=c(4,3),mar=c(4,4,2,2),oma=c(2,3,1,1))
-		if (p==4) par(mfrow=c(4,2),mar=c(4,4,2,2),oma=c(2,3,1,1))
-		 for (i in 1:nrow(row_col_signif)){
-		  eq1<-gen_sum_stats[pop_set[[p]],stat_list[row_col_signif[i,2]]]~clim[pop_set[[p]],row_col_signif[i,1]]
-		 plot(eq1,col=point_colors[pop_set[[p]]],pch=point_shapes[pop_set[[p]]], xlab=list_bioc[row_col_signif[i,1],3], ylab=colnames(gen_sum_stats)[stat_list[row_col_signif[i,2]]])
-		 abline(lm(eq1))
-		 mtext(paste("R2= ",round(unlist(summary(lm(eq1))[8]),3),sep=""),side=3)
-		  #text(eq1, labels = gen_sum_stats[pop_set[[p]],2]) #optional to add labels for pop names
+		par(mfrow=c(ceiling(nrow(row_col_signif)/4),plot_col),mar=c(4,4,2,2),oma=c(2,3,1,1))
+		for (i in 1:nrow(row_col_signif)){
+			eq1<-gen_sum_stats[pop_set[[p]],stat_list[row_col_signif[i,2]]]~clim_reg[pop_set[[p]],row_col_signif[i,1]]
+			 plot(eq1,col=point_colors[pop_set[[p]]],pch=point_shapes[pop_set[[p]]], xlab=list_bioc[row_col_signif[i,1],3], ylab=colnames(gen_sum_stats)[stat_list[row_col_signif[i,2]]])
+			 abline(lm(eq1))
+			 mtext(paste("R2= ",round(unlist(summary(lm(eq1))[8]),3),sep=""),side=3)
+			  #text(eq1, labels = gen_sum_stats[pop_set[[p]],2]) #optional to add labels for pop names
 		 }
 		dev.off() 
 	}
 
-	 if (p!=4){
+	
 	#identify rows and columns with significant p values 
-		row_col_signif<-which( matrix(p.adjust(reg_pval,"BY"),nrow=12,ncol=5)<0.05,arr.ind=T)
-	#store the list of which variables are significant and what genetic statistic
+		row_col_signif<-which( matrix(p.adjust(reg_pval,"BY"),nrow=length(clim_to_keep),ncol=5)<0.05,arr.ind=T)
+	 if (length(row_col_signif)!=0){
+	 #store the list of which variables are significant and what genetic statistic
 		variables_id<-matrix(nrow=length(row_col_signif[,1]),ncol=2)
 		for (l in 1:length(row_col_signif[,1])) variables_id[l,1]<-as.character(list_bioc[row_col_signif[l,1],3])
 		for (l in 1:length(row_col_signif[,1])) variables_id[l,2]<-colnames(gen_sum_stats)[stat_list[row_col_signif[l,2]]]
 		write.csv(variables_id[order(variables_id[,1]),],file=paste0("clim_variables_signif_",region,"strict.csv"))
 	 
-		  if (p==1) {pdf(width=10, height=4,file=paste0("regr_gen_clim_strict_",region,".pdf")); par(mfrow=c(1,4))}
-		  if (p==2) {pdf(width=10, height=10,file=paste0("regr_gen_clim_strict_",region,".pdf")); par(mfrow=c(3,3),mar=c(4,4,2,2),oma=c(2,3,1,1))}
-		  if (p==3) {pdf(width=10, height=4,file=paste0("regr_gen_clim_strict_",region,".pdf")); par(mfrow=c(1,3))}
-		 for (i in 1:nrow(row_col_signif)){
-			 eq1<-gen_sum_stats[pop_set[[p]],stat_list[row_col_signif[i,2]]]~clim[pop_set[[p]],row_col_signif[i,1]]
+		pdf(width=10, height=4,file=paste0("regr_gen_clim_strict_",region,".pdf"))
+		par(mfrow=c(ceiling(nrow(row_col_signif)/4),plot_col),mar=c(4,4,2,2),oma=c(2,3,1,1))
+		for (i in 1:nrow(row_col_signif)){
+			 eq1<-gen_sum_stats[pop_set[[p]],stat_list[row_col_signif[i,2]]]~clim_reg[pop_set[[p]],row_col_signif[i,1]]
 			 plot(eq1,col=point_colors[pop_set[[p]]],pch=point_shapes[pop_set[[p]]], xlab=list_bioc[row_col_signif[i,1],3], ylab=colnames(gen_sum_stats)[stat_list[row_col_signif[i,2]]])
 			 abline(lm(eq1))
 			 mtext(paste("R2= ",round(unlist(summary(lm(eq1))[8]),3),sep=""),side=3)
